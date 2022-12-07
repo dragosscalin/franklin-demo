95bcf7bccd4cde586bd4e4fbbfe69a2610bc5e96
Cristian CIBU <ccibu@adobe.com>
2022-12-07 16:07:42 +0200

    posting data

--- blocks/simple-choices/simple-choices.js
+++ blocks/simple-choices/simple-choices.js
@@ -1,11 +1,27 @@
 const recordOption = (text) => {
-  console.log(text);
+  const payload = {
+    data: {
+      timestamp: new Date(),
+      context: new URL(window.location.href).searchParams.get('context'),
+      feature: document.querySelector('meta[name="feature"]').content,
+      feedback: text,
+    },
+  };
+
+  fetch('/feedback-form', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  });
+
   return false;
 };
 
 export default function decorate(block) {
   block.querySelectorAll(':scope div a').forEach((child) => {
     child.href = '#';
-    child.addEventListener('click',() => recordOption(child.text));
+    child.addEventListener('click', () => recordOption(child.text));
   });
 }
